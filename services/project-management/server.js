532b1b556c095326e83910c22cf77bfea3b8af12
Dillon John <dillon.a.john@gmail.com>
2024-10-05 01:03:25 -0400

    Project management backend service initial setup
    - Add project model
    - Add RESTful routes for projects
    - Add project controller
    - Add express server
    - Add MongoDB connection

---
+++ services/project-management/server.js
@@ -0,0 +1,45 @@
+// services/project-management/server.js
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const projectRoutes = require('./src/routes/projectRoutes');
+const app = express();
+
+const allowedOrigins = [
+  process.env.CORS_ORIGIN_FRONTEND_PROJECT_MANAGEMENT,
+  process.env.CORS_ORIGIN_FRONTEND_TMS,
+  process.env.CORS_ORIGIN_USER_MANAGEMENT,
+  process.env.CORS_ORIGIN_TALENT_MANAGEMENT,
+  process.env.CORS_ORIGIN_NOTIFICATIONS,
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
+
+// Middleware for parsing JSON bodies
+app.use(express.json());
+
+// MongoDB connection setup
+mongoose
+  .connect(process.env.DATABASE_URI, {})
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+// Use project management routes
+app.use('/', projectRoutes);
+
+// Define a port
+const PORT = process.env.PORT || 5003;
+
+// Start the server after MongoDB connection is established
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is open');
+  app.listen(PORT, () => {
+    console.log(`Project Managment Service is running on port ${PORT}`);
+  });
+});
---
+++ services/project-management/src/controllers/projectController.js
@@ -0,0 +1,61 @@
+const Project = require('../models/projectModel');
+
+// Get all projects
+exports.getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find().populate('teamMembers');
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Get a single project by ID
+exports.getOneProjectById = async (req, res) => {
+  try {
+    const project = await projects.find(
+      (p) => p.id === parseInt(req.params.id)
+    );
+    if (!project) return res.status(404).send('Project not found');
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Create a new project
+exports.addProject = async (req, res) => {
+  try {
+    const newProject = new Project(req.body);
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Update an existing project
+exports.updateProject = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedProject = await Project.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedProject) return res.status(404).send('Project not found');
+    res.json(updatedProject);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Delete a project
+exports.deleteProject = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedProject = await Project.findByIdAndDelete(id);
+    if (!deletedProject) return res.status(404).send('Project not found');
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
