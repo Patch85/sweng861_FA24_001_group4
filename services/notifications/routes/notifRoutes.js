22543065681de51fe79c985ba0a15355ed493d3f
David Luo <dql5457@psu.edu>
2024-10-07 21:51:10 -0400

    Fix route calls

--- services/notifications/routes/notifRoutes.js
+++ services/notifications/routes/notifRoutes.js
@@ -3,15 +3,15 @@ const notifController = require('../controllers/notifController');
 const router = express.Router();
 
 // Send a new notification
-router.post('/', notifController.sendNotification);
+router.post('/', notifController.sendNotif);
 
 // Get all notifications for a user
-router.get('/:userId', notifController.getNotifications);
+router.get('/:userId', notifController.getNotifs);
 
 // Mark a notification as read
 router.put('/:id/read', notifController.markAsRead);
 
 // Delete a notification
-router.delete('/:id', notifController.deleteNotification);
+router.delete('/:id', notifController.deleteNotif);
 
 module.exports = router;
